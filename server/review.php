ba5f6940ca0162644b09d580879ed4e86888223d
DeadBlazze <pgejmer171@gmail.com>
2025-06-26 01:06:46 +0300

    вы уже отправляли отзыв

--- server/review.php
+++ server/review.php
@@ -7,7 +7,19 @@ if (!array_key_exists('id_user', $_SESSION)) {
 $id_user = $_SESSION['id_user'];
 $text = $_POST['text'];
 $mark = $_POST['mark'];
+
+$stmt = $mysql->prepare("SELECT * FROM reviews WHERE id_user = ? AND STATUS = 'одобрен' OR STATUS = 'на рассмотрении'");
+$stmt->bind_param("i", $id_user);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    echo 'вы уже отправляли отзыв';
+    exit;
+}
+
 $stmt = $mysql->prepare("INSERT INTO reviews (`id_user`, `text`, `mark`) VALUES (?,?,?)");
 $stmt->bind_param("isi", $id_user, $text, $mark);
 $stmt->execute();
+
 header('Location: ' . '../pages/index.php');
