f3dd46f02009c1344aecc1dff0718cc47b17009d
danielloXXI <cybiran_ez@mail.ru>
2025-06-25 22:46:58 +0300

    Дамп БД

--- db_bsk29 (2).sql
+++ db_bsk29 (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: MySQL-8.2
--- Время создания: Июн 20 2025 г., 19:19
+-- Время создания: Июн 25 2025 г., 22:46
 -- Версия сервера: 8.2.0
 -- Версия PHP: 8.1.28
 
@@ -45,9 +45,22 @@ CREATE TABLE `applications` (
 --
 
 INSERT INTO `applications` (`id_application`, `id_user`, `address`, `tel`, `status`, `date`, `serviceType`, `reason`, `payment`, `quantity`) VALUES
-(18, 14, 'Морской порт 2', '+79000000000', 'выполнена', '2025-04-12 01:27:00', 'Берёза', NULL, 'банковская карта', 4),
 (19, 14, 'awdawd', '+79581678587', 'в работе', '2025-05-31 03:07:00', 'Берёза', NULL, 'банковская карта', 2),
-(20, 14, 'Дачная 52', '+79581678587', 'в работе', '2025-06-28 23:44:00', 'Берёза', NULL, 'наличные', 5);
+(21, 15, 'Дачная 55', '+79009182652', 'в работе', '2025-06-27 01:50:00', 'Дуб', NULL, 'банковская карта', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id_reviews` int NOT NULL,
+  `id_user` int NOT NULL,
+  `text` text NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'На рассмотрении',
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -75,7 +88,8 @@ INSERT INTO `users` (`id_user`, `login`, `password`, `tel`, `email`, `FIO`) VALU
 (11, 'adminka', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '+70000000000', '123@d', 'Админ Админович'),
 (12, 'ggsel22', 'a0f5b20694e4247d5bab10ffa573c417a86c1a1989b5d5a9d0b8ef439f7b4caf', '+79009182652', 'ggsel22@mail.ru', 'Даниил Дмитриевич Антонов'),
 (13, 'ggsel222', 'fbf6e04c950646054e0333f7a15345eefc7f411dd0bb3a436a12f4bca4592fdb', '+79581678582', 'ggsel222@mail.ru', 'Даниил '),
-(14, 'cybiran', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '+79581678587', 'cybiran_ez@mail.ru', 'Даниил');
+(14, 'cybiran', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '+79581678587', 'cybiran_ez@mail.ru', 'Даниил'),
+(15, 'user', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '+79581638587', 'cybiran@mail.ru', 'ДАниил');
 
 -- --------------------------------------------------------
 
@@ -93,10 +107,9 @@ CREATE TABLE `wood` (
 --
 
 INSERT INTO `wood` (`id_wood`, `wood`) VALUES
-(1, 'Дуб'),
-(2, 'Сосна'),
-(3, 'Берёза'),
-(4, 'Ель');
+(8, 'Сосна'),
+(9, 'Берёза'),
+(12, 'Дуб');
 
 --
 -- Индексы сохранённых таблиц
@@ -108,6 +121,13 @@ INSERT INTO `wood` (`id_wood`, `wood`) VALUES
 ALTER TABLE `applications`
   ADD PRIMARY KEY (`id_application`),
   ADD KEY `id_user` (`id_user`) USING BTREE;
+
+--
+-- Индексы таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id_reviews`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Индексы таблицы `users`
@@ -130,19 +150,25 @@ ALTER TABLE `wood`
 -- AUTO_INCREMENT для таблицы `applications`
 --
 ALTER TABLE `applications`
-  MODIFY `id_application` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_application` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT для таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id_reviews` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT для таблицы `wood`
 --
 ALTER TABLE `wood`
-  MODIFY `id_wood` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_wood` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -153,6 +179,12 @@ ALTER TABLE `wood`
 --
 ALTER TABLE `applications`
   ADD CONSTRAINT `applications_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
